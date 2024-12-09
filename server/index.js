d55d25e5758989a3064649ac2a2897c6ffd45620
Mohamed-bek <mohamedbekkouche0302@gmail.com>
2024-12-09 06:57:15 +0100

    v2

--- server/index.js
+++ server/index.js
@@ -33,13 +33,13 @@ app.use("/project", ProjectRouter);
 app.use("/participant", ParticipantRouter);
 app.use(AdminRouter);
 
-const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/SCC";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose
   .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });

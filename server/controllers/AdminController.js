54ae4dc3c3adaabe0fcbd32ad2244cf06a5052ff
Mohamed-bek <mohamedbekkouche0302@gmail.com>
2024-12-09 07:04:29 +0100

    v2

--- server/controllers/AdminController.js
+++ server/controllers/AdminController.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import Admin from "../models/Admin.js";
 import { generateTokens } from "../utilitis/tokens.js";
 import cloudinary from "cloudinary";

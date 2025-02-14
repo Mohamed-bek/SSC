ed8fee4a5a0c3122c8c1c8dbdc369a9b64035d25
Mohamed-bek <mohamedbekkouche0302@gmail.com>
2025-02-14 13:27:51 +0100

    v2

--- server/index.js
+++ server/index.js
@@ -48,9 +48,5 @@ mongoose
   .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
-const PORT = process.env.PORT;
 
 export default app;
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });

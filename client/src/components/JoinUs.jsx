d0f38203aac29633d9f5e764ee7b8e3048b70a76
Mohamed-bek <mohamedbekkouche0302@gmail.com>
2025-02-24 06:33:33 +0100

    v1

--- client/src/components/JoinUs.jsx
+++ client/src/components/JoinUs.jsx
@@ -10,11 +10,11 @@ function JoinUs() {
     }
   }, []);
   return (
-    <div className=" relative min-h-dvh md:h-dvh md:pl-[10%] w-full bg-therd flex items-center pb-10 pt-20 text-secondary overflow-hidden">
-      <div className=" absolute top-0 left-0 w-full h-full">
+    <div className=" relative h-fit lg:min-h-dvh md:h-dvh md:pl-[10%] w-full bg-therd flex items-center pb-10 pt-20 text-secondary overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full">
         <video
           ref={videoRef}
-          className="w-full"
+          className="h-full object-cover"
           src="/start-coding-club.mp4"
           autoPlay
           muted
@@ -22,12 +22,12 @@ function JoinUs() {
         />
       </div>
       <div className="absolute top-0 left-0 w-full h-full bg-black/55"></div>
-      <div className="w-[90%] z-10 relative max-w-xl">
+      <div className="w-[90%] z-10 relative max-w-xl lg:mx-0 mx-auto">
         <h2 className="text-[2.1rem] uppercase text-center  mx-auto md:mx-0 md:text-left sm:text-5xl w-fit leading-[2rem] lg:text-[4.5rem] flex items-center justify-center mb-5 md:mb-8 font-light scale-y-[1.5] tracking-normal  lg:leading-[4rem] font-luckiest text-wrap md:text-nowrap">
           Start Coding,
           <br className="hidden md:block" /> Star Leading
         </h2>
-        <p className="text-sm sm:px-10 md:px-0 sm:text-lg md:text-xl mb-5 font-light md:font-semibold mx-auto capitalize md:leading-7">
+        <p className="text-sm sm:px-10 md:px-0 sm:text-lg md:text-xl mb-5 font-light md:font-semibold mx-auto capitalize md:leading-7 text-center lg:text-left">
           Ready to kickstart your coding journey? 🌟 Join the Start Coding Club
           today and gain access to hands-on workshops, expert guidance, and a
           supportive community. Whether you're here to learn, create, or
